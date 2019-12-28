e9e41f3c1969e42455c79a953b8899a779ad81db
Malonyz <mapmap1234@hotmail.com>
2019-12-28 16:41:40 +0700

    lab3_2

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-    int x=2;
-    while(x<20){
-        cout << x;
-        x=x+1;
+    int x=2,y;
+    cout << "Enter y";
+    cin >> y;
+    while(x<50){
+        cout << x << endl;
+        x=x+y;
     }
     cout <<"Malony";
     return 0;
